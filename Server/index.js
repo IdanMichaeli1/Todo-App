92e3a9776fc2dbca0fd7cb1c2d567662ba325ed5
Idan Michaeli <idan.michaeli@post.runi.ac.il>
2023-06-05 09:53:36 +0300

    add PORT const

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -36,6 +37,6 @@ app.put("/todo-list/:id", (req, res) => {
   res.send(receivedData);
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
